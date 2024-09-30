242a74a5ad7835007741f96c5d372e60e7f8cab2
Vladyslav Pavlik <pavliklaw7@gmail.com>
2024-09-30 14:51:47 +0300

    text red

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
-    <p>Hello world!</p>
+    <p class="hello">Hello world!</p>
   </div>
 </template>
 
@@ -11,3 +11,9 @@ export default {
   name: 'NuxtTutorial'
 }
 </script>
+
+<style>
+.hello {
+  color: red;
+}
+</style>
